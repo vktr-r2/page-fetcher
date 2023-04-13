2d6d5c7acab36d7c75f2257c992adedb27b092bd
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-13 13:02:23 -0400

    Add functionality to handle errors and log failure status codes + log success msg

--- fetcher.js
+++ fetcher.js
@@ -28,14 +28,18 @@ const FILE = input[1]
 
 
 request(URL, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log(body.length);  Checking for bytes>>>works
+  if (error === true) {
+    console.log(error);
+  }
+  if (response.statusCode !== 200){
+    console.log('Failed with statusCode:', response && response.statusCode);
+  }
   fs.writeFile(FILE, body, err => {
     if (err) {
       console.error(err);
       return;
     }
+    console.log(`Downloaded and saved ${body.length} bytes to ${FILE}`);
   })
 })
 
