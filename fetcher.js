60c6bb5e3090604b1f7db94ca129081e3f692138
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-13 13:06:45 -0400

    Cleaned up code, added legible comments, eslinted

--- fetcher.js
+++ fetcher.js
@@ -1,54 +1,29 @@
-// It should take two command line arguments:
-
-// a URL
-// a local file path
-// It should download the resource at the URL to the local path on your machine. Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
-
-// > node fetcher.js http://www.example.edu/ ./index.html
-// Downloaded and saved 3261 bytes to ./index.html
-
 const input = (process.argv.slice(2, process.argv.length)); //Take in console input from user
              
 const fs = require('fs');                     //Import 'fs' for file writing
 const request = require('request');           //Import 'request' for http request
 
 
-if (input.length !== 2) {
-  console.log("Incorrect Input: Provide just a URL and file for data storage (node fetcher.js <URL> <FILE>)")
+if (input.length !== 2) {                     //Handle improper inputs
+  console.log("Incorrect Input: Provide just a URL and file for data storage (node fetcher.js <URL> <FILE>)");
   return;
 }
 
-const URL = input[0]
-const FILE = input[1]
-// console.log(URL, FILE);
+const URL = input[0];                          //Store proper inputs in URL and FILE variable
+const FILE = input[1];
 
-//TRY THIS: RUNNING THE REQUEST FUNCTION IN THE FILE WRITE FUNCTIONS PARAMS/ARGS.  IN REQUEST, RETURN body
-//ALSO: maybe index file has to exist first?! >> Not necissary, if file exists it will just be over written
-//MAYBE: return the body so it can be used in the writeFile func?
-
-
-request(URL, (error, response, body) => {
-  if (error === true) {
+request(URL, (error, response, body) => {     //Make HTTP request to URL.  cb function will take error, response, body params
+  if (error === true) {                       //If an error is passed as a param, log the error
     console.log(error);
   }
-  if (response.statusCode !== 200){
+  if (response.statusCode !== 200) {           //If response (object) has statusCode value other than 200, log failure with code
     console.log('Failed with statusCode:', response && response.statusCode);
   }
-  fs.writeFile(FILE, body, err => {
+  fs.writeFile(FILE, body, err => {           //If request works and body is passed into cb function, use fs.writeFile to write to FILE var
     if (err) {
       console.error(err);
       return;
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${FILE}`);
-  })
-})
-
-
-
-
-// fs.writeFile(FILE, content, err => {
-//   if (err) {
-//     console.error(err);
-//   }
-//   // file written successfully
-// });
+    console.log(`Downloaded and saved ${body.length} bytes to ${FILE}`);    //Log success message showing bytes (based on character count)
+  });
+});
